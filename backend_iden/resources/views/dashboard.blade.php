3ba443ace35432b5b965b9827357d8245411e162
CHANDY MOEUN <chandy.moeun@student.passerellesnumeriques.org>
2024-09-02 14:25:00 +0700

    LMS1-80 <clean code>

--- backend_iden/resources/views/dashboard.blade.php
+++ backend_iden/resources/views/dashboard.blade.php
@@ -1,6 +1,46 @@
 <x-app-layout>
+
     <div class="mt-20">
+
         <div class="container mx-auto px-6 py-8">
+            <!-- <div class="button" style="margin: 50px;">
+                <button onclick="ShowAlert(alertMsg)" style="background-color: #333; color:#fff; border:0; outline:0; width:120px; height:40px; margin:9px; cursor:pointer; ">alert</button>
+                <div id="taostBox" style="position: absolute; top:10%; right:2%; display:flex; align-items:flex-end; flex-direction:column; overflow:hidden; padding:20px"></div>
+                <style>
+                    .taost {
+                        background-color: #fff;
+                        width: 100%;
+                        padding: 3%;
+                        height: 5%;
+                        font-weight: 900;
+                        margin: 15px 0;
+                        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    }
+                </style>
+            </div>
+            <script>
+                let taostBox = document.getElementById('taostBox');
+                let alertMsg = 'Welcom To Addmin Dashboard. How are you?'
+
+                function ShowAlert(msg) {
+                    let taost = document.createElement('div');
+                    taost.classList.add('taost');
+                    taost.innerHTML = msg;
+                    taostBox.appendChild(taost);
+
+                    if (msg.includes('alert')){
+                        taost.classList.add('alert');
+                    }
+                    setTimeout(() => {
+                        taostBox.removeChild(taost);
+                    }, 4000);
+                }
+            </script> -->
+
+
             <div class="Container p-2 d-flex justify-content-center" style="  width:100%">
                 <div style="border-bottom: #B0B0B0 1px solid;  display: flex; justify-content:center; flex-direction: row; height:30vh; width: 90%; margin-top: 35px;">
                     <div style="margin-bottom: 10px; border-right: #B0B0B0 1px solid; width: 45%;  display: flex; justify-content:center;">
@@ -15,11 +55,11 @@
                     <div style=" width: 45%; display: flex; justify-content:center;">
                         <div class=" bg-yellow-400 " style="margin-top: 30px; margin-right: 70px; width: 50%; height:65%; display: flex; flex-direction: row; box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;">
                             <img src="../images/Leave.png" style="margin-left: 25px; width: 75px; height:9vh; display: flex; align-self:center;">
-                            <div >
+                            <div>
                                 <h5 class="font-bold mt-6 ml-3 "><b>Leaveds</b></h5>
-                                
+
                                 <p class="ml-1"><em>This week : </em></p>
-                                
+
                             </div>
                         </div>
                     </div>
@@ -124,7 +164,6 @@
                 <div class="container mt-5">
                     <div class="section-header">
                         <h2><b>THIS WEEK </b></h2>
-                        
                     </div>
                     <div class="table-responsive">
                         <table class="table table-striped">
