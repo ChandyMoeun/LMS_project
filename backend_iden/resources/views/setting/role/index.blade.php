87ae4295d688667d22e9529283f34ff4fff20650
CHANDY MOEUN <chandy.moeun@student.passerellesnumeriques.org>
2024-09-09 14:06:10 +0700

    LMS1-85 <Prepare sidebar good looking >

--- backend_iden/resources/views/setting/role/index.blade.php
+++ backend_iden/resources/views/setting/role/index.blade.php
@@ -1,56 +1,64 @@
 <x-app-layout>
-   <div class="mt-20">
-        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-            <div class="container mx-auto px-6 py-2">
-                <div class="text-right">
-                  @can('Role create')
-                    <a href="{{route('admin.roles.create')}}" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">New Role</a>
-                </div>
-                @endcan
+  <div class="mt-20">
+    <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+      <div class="d-flex border-b-2 border-gray-300 px-8 h-20 items-center mb-5">
+        <a href="/admin/mail#">
+          <svg class="w-6 h-6 text-gray-800 dark:text-white hover:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
+          </svg>
+        </a>
+        <h1 class="font-bold text-3xl mt-5 hover:text-yellow-400"><b>Roles </b></h1> 
+      </div>
+      <div class="container mx-auto px-6 py-2">
+        <div class="text-right">
+          @can('Role create')
+          <a href="{{route('admin.roles.create')}}" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">New Role</a>
+        </div>
+        @endcan
 
-              <div class="bg-white shadow-md rounded my-6">
-                <table class="text-left w-full border-collapse">
-                  <thead>
-                    <tr>
-                      <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light w-2/12">Role Name</th>
-                      <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light">Permissions</th>
-                      <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light text-right w-2/12">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @can('Role access')
-                      @foreach($roles as $role)
-                      <tr class="hover:bg-grey-lighter">
-                        <td class="py-4 px-6 border-b border-grey-light">{{ $role->name }}</td>
-                        <td class="py-4 px-6 border-b border-grey-light">
-                            @foreach($role->permissions as $permission)
-                              <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-500 rounded-full">{{ $permission->name }}</span>
-                            @endforeach
-                        </td>
-                        <td class="py-4 px-6 border-b border-grey-light text-right">
+        <div class="bg-white shadow-md rounded my-6">
+          <table class="text-left w-full border-collapse">
+            <thead>
+              <tr>
+                <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light w-2/12">Role Name</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light">Permissions</th>
+                <th class="py-4 px-6 bg-grey-lightest font-bold text-sm text-grey-dark border-b border-grey-light text-right w-2/12">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              @can('Role access')
+              @foreach($roles as $role)
+              <tr class="hover:bg-grey-lighter">
+                <td class="py-4 px-6 border-b border-grey-light">{{ $role->name }}</td>
+                <td class="py-4 px-6 border-b border-grey-light">
+                  @foreach($role->permissions as $permission)
+                  <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-500 rounded-full">{{ $permission->name }}</span>
+                  @endforeach
+                </td>
+                <td class="py-4 px-6 border-b border-grey-light text-right">
 
-                          @can('Role edit')
-                          <a href="{{route('admin.roles.edit',$role->id)}}" class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark text-blue-400">Edit</a>
-                          @endcan
+                  @can('Role edit')
+                  <a href="{{route('admin.roles.edit',$role->id)}}" class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark text-blue-400">Edit</a>
+                  @endcan
 
-                          @can('Role delete')
-                          <form action="{{ route('admin.roles.destroy', $role->id) }}" method="POST" class="inline">
-                              @csrf
-                              @method('delete')
-                              <button class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark text-red-400">Delete</button>
-                          </form>
-                          @endcan
+                  @can('Role delete')
+                  <form action="{{ route('admin.roles.destroy', $role->id) }}" method="POST" class="inline">
+                    @csrf
+                    @method('delete')
+                    <button class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark text-red-400">Delete</button>
+                  </form>
+                  @endcan
 
-                        </td>
-                      </tr>
-                      @endforeach
-                    @endcan
-                  </tbody>
-                </table>
-              </div>
-  
-            </div>
-        </main>
-    </div>
-</div>
+                </td>
+              </tr>
+              @endforeach
+              @endcan
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+    </main>
+  </div>
+  </div>
 </x-app-layout>
--- backend_iden/resources/views/setting/setting/mail.blade.php
+++ backend_iden/resources/views/setting/setting/mail.blade.php
@@ -2,63 +2,119 @@
   <div class="mt-10">
     <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200 p-5">
       <div class="container flex flex-col gap-16 mx-auto p-4 px-1  justify-evenly ">
-        <div class="flex border-b-2 border-gray-300 px-8 h-20 items-center">
+        <div class="d-flex border-b-2 border-gray-300 px-8 h-20 items-center">
           <h1 class="font-bold text-3xl mt-5 hover:text-yellow-400"><b>Setting </b></h1>
         </div>
-        
-        <div class="flex gap-5 justify-evenly">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Departments</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">They can see and do everything best not to have many with this role.</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
-              View more
-              <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a>
+        <div class="flex flex-wrap justify-center">
+
+          <!-- card departments -->
+          <div class="p-4 max-w-sm">
+            <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                <a href="department">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Departments</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Crete, Update, and delete Department.</p>
+                <a href="department" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                  View more
+                  <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Leave Types</h5>
-            </a>
-            <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400">Admin to help sort stuff, but have less access
-              to confidential information like salaries.</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
-              View more
-              <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a>
+
+          <!-- card leave type -->
+          <div class="p-4 max-w-sm">
+            <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                <a href="leavetype">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Leave Types</h5>
+                </a>
+                <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400">Admin to help sort stuff, but have less access
+                  to confidential information like salaries.</p>
+                <a href="leavetype" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                  View more
+                  <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="flex gap-5 justify-evenly">
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Rejected Leave</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">You can see the leave that you are rejected
-              here and you also can change to approve.</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
-              View more
-              <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a>
+          <!-- card reset password -->
+          <div class="p-4 max-w-sm">
+            <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Reset Password</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Admin and manager can reset new password for
+                  each employee.</p>
+                  <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                    View more
+                    <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <!-- card rejected leave -->
+            <div class="p-4 max-w-sm">
+              <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                  <a href="position">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Position</h5>
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">You can see the leave that you are rejected
+                    here and you also can change to approve.</p>
+                  <a href="position" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                    View more
+                    <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+          <!-- card rejected leave -->
+          <div class="p-4 max-w-sm">
+            <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                <a href="{{ route('admin.permissions.index') }}">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Permission</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">You can see the leave that you are rejected
+                  here and you also can change to approve.</p>
+                <a href="{{ route('admin.permissions.index') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                  View more
+                  <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Reset Password</h5>
-            </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Admin and manager can reset new password for
-              each employee who forgot there password.</p>
-            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
-              View more
-              <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-              </svg>
-            </a>
+          <!-- card rejected leave -->
+          <div class="p-4 max-w-sm">
+            <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
+              <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 ">
+                <a href="{{ route('admin.roles.index') }}">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Role</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">You can see the leave that you are rejected
+                  here and you also can change to approve.</p>
+                <a href="{{ route('admin.roles.index') }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg">
+                  View more
+                  <svg class="rtl:rotate-180 ml-2 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                  </svg>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
