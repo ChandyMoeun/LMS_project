d34c8bacd8ede56588858a82af5eba5479c39c94
CHANDY MOEUN <chandy.moeun@student.passerellesnumeriques.org>
2024-09-16 10:50:36 +0700

    LMS1-209 <update CRUD on Role page>

--- backend_iden/resources/views/setting/role/new.blade.php
+++ backend_iden/resources/views/setting/role/new.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
-  <div class="mt-20">
+  <div class="mt-10 p-5">
     <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+      <div class="d-flex border-b-2 border-gray-300 px-8 h-20 items-center mb-5">
+        <a href="/admin/roles">
+          <svg class="w-6 h-6 mt-5 text-gray-800 dark:text-white hover:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
+          </svg>
+        </a>
+        <h1 class="font-bold text-3xl mt-5 hover:text-yellow-400 w-2/12"><b>New Role</b></h1> 
+      </div>
       <div class="container mx-auto px-6 py-1">
         <div class="bg-white shadow-md rounded my-6 p-5">
-          <a href="/admin/roles">
-            <svg class="w-5 h-6 text-gray-800 dark:text-white hover:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
-            </svg>
-          </a>
           <form method="POST" action="{{ route('admin.roles.store')}}">
             @csrf
             @method('post')
-            <div class="flex flex-col space-y-2">
+            <div class="flex flex-col space-y-2 m-5">
               <label for="role_name" class="text-gray-700 select-none font-medium">Role Name</label>
               <input
                 id="role_name"
@@ -22,8 +25,8 @@
                 class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" />
             </div>
 
-            <h3 class="text-xl my-4 text-gray-600">Permissions</h3>
-            <div class="grid grid-cols-3 gap-4">
+            <strong><h1 class="text-xl text-center mt-10 my-4 text-gray-600">Permissions</h1></strong>
+            <div class="grid grid-cols-3 gap-4 m-10">
               @foreach($permissions as $permission)
               <div class="flex flex-col justify-cente">
                 <div class="flex flex-col">
@@ -34,8 +37,8 @@
               </div>
               @endforeach
             </div>
-            <div class="text-center mt-16">
-              <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">Submit</button>
+            <div class="text-center mt-5">
+              <button type="submit" class="bg-black text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-yellow-400 transition-colors">Submit</button>
             </div>
         </div>
 
