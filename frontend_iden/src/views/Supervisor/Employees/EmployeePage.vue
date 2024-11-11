123cdbcd76ee7aa31cc61aca663355efd5236fd3
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-11-11 10:47:43 +0700

    get team employees and detail information

--- frontend_iden/src/views/Supervisor/Employees/EmployeePage.vue
+++ frontend_iden/src/views/Supervisor/Employees/EmployeePage.vue
@@ -9,19 +9,43 @@
         <main class="bg-gray sticky top-0">
           <div class="mt-10">
             <!-- Employee Management Header -->
-            <div class="d-flex text-black" style="display: flex; flex-direction: column; border-bottom: solid 1px gray" >
-              <h1 class="font-bold text-3xl px-8 hover:text-yellow-500 w-4/12"> <b>Team Management</b> </h1>
-              <p class="px-8">Total members: {{ filteredEmployees.length }}</p>
+            <div
+              class="d-flex text-black"
+              style="display: flex; flex-direction: column; border-bottom: solid 1px gray"
+            >
+              <h1 class="font-bold text-3xl px-8 hover:text-yellow-500 w-4/12">
+                <b>Team Management</b>
+              </h1>
+              <p class="px-8">Total members: {{ teamCount }}</p>
             </div>
 
             <!-- Search and Filter select position-->
             <div class="flex justify-between mt-20 mb-7">
-              <input v-model="searchQuery" type="text" placeholder="Search..." title="Type of leave or approver Name" class="w-2/6 h-9 px-2 border rounded rounded-lg shadow-md"/>
+              <input
+                v-model="searchQuery"
+                type="text"
+                placeholder="Search..."
+                title="Type of leave or approver Name"
+                class="w-2/6 h-9 px-2 border rounded rounded-lg shadow-md"
+              />
               <div class="flex items-center justify-end space-x-2 w-2/5">
-                <button class="p-2 flex items-center border-none bg-gray-900 text-white font-bold h-9 px-2 py-1 rounded focus:outline-none shadow hover:bg-yellow-500 transition-colors"
-                  @click="addEmployee" >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                <button
+                  class="p-2 flex items-center border-none bg-gray-900 text-white font-bold h-9 px-2 py-1 rounded focus:outline-none shadow hover:bg-yellow-500 transition-colors"
+                  @click="addEmployee"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6 mr-2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+                    />
                   </svg>
                   <a href="/supervisor/employee/create" class="no-underline text-white">Add</a>
                 </button>
@@ -30,7 +54,7 @@
 
             <!-- Employee Table -->
             <div class="overflow-x-auto">
-              <table class="w-full bg-white shadow-md rounded">
+              <table v-if="authStore.member" class="w-full bg-white shadow-md rounded">
                 <thead>
                   <tr class="header bg-black text-white">
                     <th class="p-3 text-left">Staff_id</th>
@@ -42,23 +66,35 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr v-for="employee in filteredEmployees" :key="employee.id" class="bg-gray-100 border-b border-gray-200">
-                    <td class="p-3">{{ employee.staff_id }}</td>
+                  <tr
+                    v-for="(member, index) in authStore.member"
+                    :key="index"
+                    class="bg-gray-100 border-b border-gray-200"
+                  >
+                    <td class="p-3">{{ member.staff_id }}</td>
                     <td class="p-3">
-                      <img :src=" employee.profile ? `/images/${employee.profile}` : '/images/default_profile.png' "
-                        alt="Profile" class="w-12 h-12 rounded-full object-cover"/>
+                      <img
+                        :src="
+                          member.profile && member.profile
+                            ? `http://127.0.0.1:8000/images/${member.profile}`
+                            : '/images/default-profile.jpg'
+                        "
+                        alt="Profile Picture"
+                        class="w-14 h-14 rounded-full"
+                      />
                     </td>
-                    <td class="p-3 text-center">{{ employee.full_name }}</td>
-                    <td class="p-3 text-blue-600 text-center">{{ employee.email }}</td>
+                    <td class="p-3 text-center">{{ member.full_name }}</td>
+                    <td class="p-3 text-blue-600 text-center">{{ member.email }}</td>
                     <td class="p-3 text-center">
-                      {{ employee.position ? employee.position.name : 'No position' }}
+                      {{ member.position.name }}
                     </td>
                     <td class="text-center w-3/12">
-                      <button @click="viewEmployee(employee.id)"
-                        class="text-white px-2 py-1 border-solid border-0 border-indigo-600 rounded-lg bg-blue-600 hover:bg-blue-400 border-none">
-                        <a href="/supervisor/employee/profile/more" class="no-underline text-white">More</a>
-                      </button>
-                      <!-- <button @click="editEmployee(employee.id)" class="ml-2 text-white px-2 py-1 border-solid border-1 border-indigo-600 rounded-lg bg-gray-900 hover:bg-yellow-400">Update</button> -->
+                      <router-link
+                        :to="{ name: 'EmployeeProfile', params: { id: member.id } }"
+                        class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition duration-200 shadow-sm no-underline"
+                      >
+                        More
+                      </router-link>
                     </td>
                   </tr>
                 </tbody>
@@ -72,9 +108,10 @@
 </template>
 
 <script>
-import axiosInstance from '@/plugins/axios'
+// import axiosInstance from '@/plugins/axios'
 import SupervisorSidebar from '@/Components/SupervisorSidebar.vue'
 import WebHeaderMenu from '@/Components/WebHeaderMenu.vue'
+import { useAuthStore } from '@/stores/get-member'
 
 export default {
   components: { SupervisorSidebar, WebHeaderMenu },
@@ -90,9 +127,10 @@ export default {
   computed: {
     // Computed property to filter employees based on searchQuery
     filteredEmployees() {
-      return this.employees_list.filter(employee =>
-        employee.full_name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-        employee.staff_id.includes(this.searchQuery)
+      return this.employees_list.filter(
+        (employee) =>
+          employee.full_name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          employee.staff_id.includes(this.searchQuery)
       )
     }
   },
@@ -104,7 +142,17 @@ export default {
       } catch (error) {
         console.error(error)
       }
-    },
+    }
+  },
+  setup() {
+    const authStore = useAuthStore()
+
+    // Fetch member data when component is mounted
+    // onMounted(() => {
+    //   authStore.fetchMember() // Call the fetchMember method from the store
+    // })
+
+    return { authStore, teamCount: authStore.teamCount }
   }
 }
 </script>
--- frontend_iden/src/views/Web/Post/ListView.vue
+++ frontend_iden/src/views/Web/Post/ListView.vue
@@ -1,142 +1,66 @@
 <template>
-  <WebLayout>
-    <div class="container mt-4 p-4 bg-white shadow rounded">
-      <div class="text-center mb-4">
-        <p class="fs-4 fw-bold">Your journey to excellence starts here.</p>
-        <p>Join Us Today and Get Started</p>
-      </div>
-
-      <!-- Display Positions -->
-      <h2 class="text-center mb-4">Positions</h2>
-      <table class="min-w-full bg-gray-100 border border-gray-100 mb-4">
-        <thead>
-          <tr class="bg-black text-white text-xs">
-            <th class="py-2 px-2 text-center border-b">Position Name</th>
-            <th class="py-2 px-2 text-center border-b">Department ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- Loop through positions and display them in table rows -->
-          <tr
-            v-for="position in positionStore.positions"
-            :key="position.id"
-            class="hover:bg-white text-xs"
-          >
-            <td class="py-2 px-2 text-center border-b">{{ position.position }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ position.department }}</td>
-          </tr>
-        </tbody>
-      </table>
-
-      <!-- Display Departments -->
-      <h2 class="text-center mb-4">Departments</h2>
-      <table class="min-w-full bg-gray-100 border border-gray-100 mb-4">
-        <thead>
-          <tr class="bg-black text-white text-xs">
-            <th class="py-2 px-2 text-center border-b">Department Name</th>
-            <th class="py-2 px-2 text-center border-b">Manager ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="department in departmentStore.departments"
-            :key="department.id"
-            class="hover:bg-white text-xs"
-          >
-            <td class="py-2 px-2 text-center border-b">{{ department.name}}</td>
-            <td class="py-2 px-2 text-center border-b">{{ department.manager_name }}</td>
-            <!-- <td v-for="position in department" :key="position.id" class="py-2 px-2 text-center border-b">{{ department}}</td> -->
-          </tr>
-        </tbody>
-      </table>
-
-      <!-- Display Leave Requests -->
-      <h2 class="text-center mb-4">Leave Requests</h2>
-      <table class="min-w-full bg-gray-100 border border-gray-100">
-        <thead>
-          <tr class="bg-black text-white text-xs">
-            <th class="py-2 px-2 text-center border-b">Staff_ID</th>
-            <th class="py-2 px-2 text-center border-b">Name</th>
-            <th class="py-2 px-2 text-center border-b">Leave Type</th>
-            <th class="py-2 px-2 text-center border-b">Half Day Type</th>
-            <th class="py-2 px-2 text-center border-b">Start Time</th>
-            <th class="py-2 px-2 text-center border-b">End Time</th>
-            <th class="py-2 px-2 text-center border-b">Attachment</th>
-            <th class="py-2 px-2 text-center border-b">Total Request Days</th>
-            <th class="py-2 px-2 text-center border-b">From Date</th>
-            <th class="py-2 px-2 text-center border-b">To Date</th>
-            <th class="py-2 px-2 text-center border-b">Status</th>
-            <th class="py-2 px-2 text-center border-b">Approved By</th>
-            <th class="py-2 px-2 text-center border-b">Rejected By</th>
-            <th class="py-2 px-2 text-center border-b">Reason</th>
-            <th class="py-2 px-2 text-center border-b">Duration</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="leaveRequest in leaveRequestStore.leaveRequests"
-            :key="leaveRequest.id"
-            class="hover:bg-white text-xs"
-          >
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.employee.staff_id }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.employee.full_name }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.leave_type.leave_name }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.half_day_type }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.start_time }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.end_time }}</td>
-            <td class="py-2 px-2 text-center border-b">
-              <img
-                v-if="leaveRequest.attachment"
-                :src="leaveRequest.attachment"
-                alt="Attachment"
-                class="w-20 h-20 object-cover"
-              />
-              <span v-else>No Image</span>
-            </td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.total_requested_days }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.from_date }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.to_date }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.status }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.approved_by }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.rejected_by }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.reason }}</td>
-            <td class="py-2 px-2 text-center border-b">{{ leaveRequest.duration }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </WebLayout>
+  <div>
+    <h1>Team Members</h1>
+    <h1>Member of Team {{ teamCount }}</h1>
+    <table v-if="authStore.member" class="min-w-full table-auto border-collapse">
+      <thead>
+        <tr>
+          <th class="border px-4 py-2">Staff ID</th>
+          <th class="border px-4 py-2">Profile</th>
+          <th class="border px-4 py-2">Name</th>
+          <th class="border px-4 py-2">Gender</th>
+          <th class="border px-4 py-2">Email</th>
+          <th class="border px-4 py-2">Date of Birth</th>
+          <th class="border px-4 py-2">Position</th>
+          <th class="border px-4 py-2">Department</th>
+          <th class="border px-4 py-2">Joined Date</th>
+          <th class="border px-4 py-2">Confirm Date</th>
+          <th class="border px-4 py-2">Supervisor</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(member, index) in authStore.member" :key="index">
+          <td class="border px-4 py-2">{{ member.staff_id }}</td>
+          <td class="border px-4 py-2">
+            <img
+              :src="
+                member.profile && member.profile
+                  ? `http://127.0.0.1:8000/images/${member.profile}`
+                  : '/images/default-profile.jpg'
+              "
+              alt="Profile Picture"
+              class="w-16 h-16 rounded-full"
+            />
+          </td>
+          <td class="border px-4 py-2">{{ member.full_name }}</td>
+          <td class="border px-4 py-2">{{ member.gender }}</td>
+          <td class="border px-4 py-2">{{ member.email }}</td>
+          <td class="border px-4 py-2">{{ member.dob }}</td>
+          <td class="border px-4 py-2">{{ member.position.name }}</td>
+          <td class="border px-4 py-2">{{ member.department.name }}</td>
+          <td class="border px-4 py-2">{{ member.joined_date }}</td>
+          <td class="border px-4 py-2">{{ member.entitled_date }}</td>
+          <td class="border px-4 py-2">{{ member.manager.full_name }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import { useDepartmentStore } from '@/stores/department'
-import { useLeaveRequestStore } from '@/stores/leave-request'
-import { usePositionStore } from '@/stores/all-positions'
-import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/get-member' // Make sure the path is correct
+// import { onMounted } from 'vue'
 
 export default {
   setup() {
-    const departmentStore = useDepartmentStore()
-    const leaveRequestStore = useLeaveRequestStore()
-    const positionStore = usePositionStore()
+    const authStore = useAuthStore()
 
-    // Fetch data on component mount
-    onMounted(() => {
-      departmentStore.fetchDepartments() // Fetch departments
-      leaveRequestStore.fetchLeaveRequests() // Fetch leave requests
-      positionStore.fetchPositions() // Fetch positions
-      console.log(positionStore.fetchPositions())
-    })
+    // Fetch member data when component is mounted
+    // onMounted(() => {
+    //   authStore.fetchMember() // Call the fetchMember method from the store
+    // })
 
-    return {
-      departmentStore,
-      leaveRequestStore,
-      positionStore // Access positions from store
-    }
+    return { authStore,teamCount: authStore.teamCount, }
   }
 }
 </script>
-
-<style>
-/* You can add custom styles here */
-</style>
